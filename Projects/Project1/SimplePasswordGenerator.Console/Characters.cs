08fadcbec6c5ef9d7eadce61d3736bee27c5dcb6
Ayça Akbaş <121265757+aycakbas@users.noreply.github.com>
2023-02-01 23:56:29 +0300

    Projects1
    SimplePasswordGenerator

---
+++ Projects/Project1/SimplePasswordGenerator.Console/Characters.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePasswordGenerator.Console
+{
+    public class Characters
+    {
+        public static string Numbers = "1234567890";
+        public static string LowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+        public static string UppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string SpecialCharacters = "*$-+?_&=!%{}/\"";
+
+        private readonly Random _random;
+
+        public static string SelectedChars { get; set; } = null!;
+
+        public Characters()
+        {
+            _random = new Random();
+        }
+
+        public static void SelectNumbers ()
+        {
+            SelectedChars += (Numbers);
+        }
+        public static void SelectLovercaseChars()
+        {
+            SelectedChars += (LowercaseCharacters);
+        }
+        public static void SelectUppercaseChars()
+        {
+            SelectedChars += (UppercaseCharacters);
+        }
+        public static void SelectSpecialChars()
+        {
+            SelectedChars += (SpecialCharacters);
+        }
+        public string GetRandomPassword (int x)
+        {
+            string charset = "";
+
+            for (int i=0; i<x; i++) 
+            {
+                var randomIndex = _random.Next(x);
+                var character = SelectedChars[randomIndex];
+                charset += character;
+            }
+            return charset;
+        }
+
+
+    }
+}
---
+++ Projects/Project1/SimplePasswordGenerator.Console/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SimplePasswordGenerator.Console;
+using System.Collections;
+using System.ComponentModel.Design;
+
+string myText = "Welcome to the B E S T P A S S W O R D M A N A G E R !";
+int lengthText = myText.Length;
+
+Console.WriteLine(new string('*', lengthText) + "\n" + myText + "\n" + new string('*', lengthText));
+
+Console.WriteLine("Do you want to Include Number?");
+string includeNumbers = Console.ReadLine();
+
+switch (includeNumbers)
+{
+    case "Y":
+        Characters.SelectNumbers();
+        break;
+
+    case "y":
+        Characters.SelectNumbers(); break;
+
+    default:
+        break;
+}
+
+Console.WriteLine("OK! How about lowercase characters?");
+string includeLowercaseCharacters = Console.ReadLine();
+
+switch (includeLowercaseCharacters)
+{
+    case "Y":
+        Characters.SelectLovercaseChars();
+        break;
+
+    case "y":
+        Characters.SelectLovercaseChars(); break;
+
+    default:
+        break;
+}
+
+Console.WriteLine("Very nice! How about uppercase characters?");
+string includeUppercaseCharacters = Console.ReadLine();
+
+switch (includeUppercaseCharacters)
+{
+    case "Y":
+        Characters.SelectUppercaseChars();
+        break;
+
+    case "y":
+        Characters.SelectUppercaseChars(); break;
+
+    default:
+        break;
+}
+
+Console.WriteLine("All right! We are almost done. Would you also want to add special characters?");
+string includeSpecialCharacters = Console.ReadLine();
+
+switch (includeSpecialCharacters)
+{
+    case "Y":
+        Characters.SelectSpecialChars();
+        break;
+
+    case "y":
+        Characters.SelectSpecialChars(); break;
+
+    default:
+        break;
+}
+
+var characters = new Characters();
+
+Console.WriteLine("Great! How long do you want to keep your password length?");
+int passwordLength = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(characters.GetRandomPassword(passwordLength));
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
